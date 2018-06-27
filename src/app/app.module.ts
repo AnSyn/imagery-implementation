c6d180d452dad6e7f3ebb93e814927294dff7bc6
YairTawil <yairtawil@gmail.com>
2018-06-27 18:14:11 +0300

    fix: new imagery

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
-import { ImageryModule } from '@ansyn/imagery';
 import { BrowserModule } from '@angular/platform-browser';
-import { BaseOlModule } from './providers/ol/base-ol.module';
 import { NavbarComponent } from './navbar/navbar.component';
+import { ImageryModule } from '@ansyn/imagery';
+import { OsmSourceProvider } from './providers/map-source-providers/osm.source-provider';
+import { OlImap } from './providers/maps/ol-imap';
 
 @NgModule({
   declarations: [
@@ -12,8 +13,11 @@ import { NavbarComponent } from './navbar/navbar.component';
   ],
   imports: [
     BrowserModule,
-    ImageryModule,
-    BaseOlModule
+    ImageryModule.provide({
+      maps: [OlImap],
+      plugins: [],
+      mapSourceProviders: [OsmSourceProvider]
+    }),
   ],
   providers: [],
   bootstrap: [AppComponent]
