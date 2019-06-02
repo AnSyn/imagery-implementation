7c6c976048054fa960e4ec051e91d032ec23b8b8
Tzahi Levi <thetzahilevi@gmail.com>
2019-06-02 17:17:28 +0300

    upgrade version

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -35,7 +35,7 @@ import { MccColorPickerModule } from 'material-community-components';
     {
       provide: OL_PLUGINS_CONFIG,
       useValue: {
-        Annotations:{}
+        Annotations: {}
       }
     },
     {
